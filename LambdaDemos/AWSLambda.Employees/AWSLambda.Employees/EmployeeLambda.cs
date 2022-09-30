3ceba26ebfd42bb7a03a36601f813097e8d5dd20
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-09-30 12:18:50 +0530

    Swamy/27aug2022 starting lambda (#3)
    * Initial Version of Code
    
    * Another Deployment
    
    * Changes
    
    * Renamed the folder
    
    * Removed the folder
    
    * Initial Version
    
    * Retrieving Employee Record
    
    * Update EmployeeLambda.cs
    
    * Returning Employee Response Dto
    
    * Update aws-lambda-tools-defaults.json
    
    * Update aws-lambda-tools-defaults.json
    
    * Temporary Check in

--- LambdaDemos/AWSLambda.Employees/AWSLambda.Employees/EmployeeLambda.cs
+++ LambdaDemos/AWSLambda.Employees/AWSLambda.Employees/EmployeeLambda.cs
@@ -1,7 +1,9 @@
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using AWSLambda.Employees.Models;
+using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -17,8 +19,23 @@ public class EmployeeLambda
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public async Task<EmployeeResponseDto> EmployeeHandler(EmployeeRequestDto employeeRequestDto, ILambdaContext context)
+    public async Task<APIGatewayProxyResponse> EmployeeHandler(APIGatewayProxyRequest aPIGatewayProxyRequest, ILambdaContext context)
     {
+        APIGatewayProxyResponse aPIGatewayProxyResponse = new();
+        EmployeeRequestDto employeeRequestDto = JsonSerializer.Deserialize<EmployeeRequestDto>(aPIGatewayProxyRequest.Body);
+        EmployeeResponseDto employeeResponseDto = new();
+
+        if (employeeRequestDto == null || string.IsNullOrEmpty(employeeRequestDto.employeeId))
+        {
+            context.Logger.LogDebug($"Did NOT receive the valid request for Employee.");
+
+            employeeResponseDto.Message = "Received Invalid Employee Id";
+
+            aPIGatewayProxyResponse.Body = JsonSerializer.Serialize(employeeResponseDto);
+            aPIGatewayProxyResponse.StatusCode = 400;
+            return aPIGatewayProxyResponse;
+        }
+
         context.Logger.LogDebug($"Received the request with Employee Id {employeeRequestDto.employeeId}.");
 
         var _dynamoDbContext = new DynamoDBContext(new AmazonDynamoDBClient());
@@ -26,9 +43,11 @@ public class EmployeeLambda
 
         context.Logger.LogDebug($"Sending the response for Employee Id {employeeRequestDto.employeeId}.");
 
-        EmployeeResponseDto employeeResponseDto = GenerateEmployeeResponseDto(employeeDto);
+        employeeResponseDto = GenerateEmployeeResponseDto(employeeDto);
 
-        return employeeResponseDto;
+        aPIGatewayProxyResponse.Body = JsonSerializer.Serialize(employeeResponseDto);
+        aPIGatewayProxyResponse.StatusCode = 200;
+        return aPIGatewayProxyResponse;
     }
 
     private static EmployeeResponseDto GenerateEmployeeResponseDto(EmployeeDto employeeDto)
