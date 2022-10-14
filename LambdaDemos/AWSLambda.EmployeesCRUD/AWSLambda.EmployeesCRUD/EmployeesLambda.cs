480692191caa94c4b51c6c4f9a4122a432f0e8ef
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-14 23:21:46 +0530

    Verified Retrieval

--- LambdaDemos/AWSLambda.EmployeesCRUD/AWSLambda.EmployeesCRUD/EmployeesLambda.cs
+++ LambdaDemos/AWSLambda.EmployeesCRUD/AWSLambda.EmployeesCRUD/EmployeesLambda.cs
@@ -1,4 +1,7 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.Core;
+using AWSLambda.EmployeesCRUD.Models;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -14,8 +17,42 @@ public class EmployeesLambda
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string EmployeesHandler(string employeeId, ILambdaContext context)
+    public async Task<EmployeeResponseDto> EmployeesHandler(string employeeId, ILambdaContext context)
     {
-        return employeeId.ToUpper();
+        EmployeeResponseDto employeeResponseDto = new();
+
+        if (string.IsNullOrEmpty(employeeId))
+        {
+            context.Logger.LogDebug($"Did NOT receive the valid request for Employee.");
+
+            employeeResponseDto.Message = "Received Invalid Employee Id";
+
+            return employeeResponseDto;
+        }
+
+        context.Logger.LogDebug($"Received the request with Employee Id {employeeId}.");
+
+        var _dynamoDbContext = new DynamoDBContext(new AmazonDynamoDBClient());
+        EmployeeDto employeeDto = await _dynamoDbContext.LoadAsync<EmployeeDto>(employeeId);
+
+        context.Logger.LogDebug($"Sending the response for Employee Id {employeeId}.");
+
+        employeeResponseDto = GenerateEmployeeResponseDto(employeeDto);
+
+        return employeeResponseDto;
     }
+
+    private static EmployeeResponseDto GenerateEmployeeResponseDto(EmployeeDto employeeDto)
+    {
+        EmployeeResponseDto employeeResponseDto = new();
+        if (employeeDto != null)
+        {
+            employeeResponseDto.Success = true;
+            employeeResponseDto.Message = "Record Found";
+            employeeResponseDto.Employee = employeeDto;
+        }
+
+        return employeeResponseDto;
+    }
+
 }
