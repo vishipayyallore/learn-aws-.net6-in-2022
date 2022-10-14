6baae221e3c6388efe68d03968113fe796d588bc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-14 23:04:06 +0530

    Models

---
+++ LambdaDemos/AWSLambda.EmployeesCRUD/AWSLambda.EmployeesCRUD/Models/EmployeeDto.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AWSLambda.EmployeesCRUD.Models
+{
+
+    [DynamoDBTable("employees")]
+    public class EmployeeDto
+    {
+        [DynamoDBHashKey]
+        public string employeeId { get; set; } = string.Empty;
+
+        public string name { get; set; } = string.Empty;
+
+        public int age { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal salary { get; set; }
+
+        public string designation { get; set; } = string.Empty;
+    }
+
+}
