2b58de02bc62b23ea21c9a413720765350e20127
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-27 20:01:31 +0530

    Multiple Handlers

---
+++ LambdaDemos/EmployeesCRUD/EmployeesCRUD/Employees.cs
@@ -0,0 +1,27 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace EmployeesCRUD;
+
+public class Employees
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string GetEmployeeByIdHandler(string input, ILambdaContext context)
+    {
+        return input.ToUpper();
+    }
+
+    public string GetAllEmployeesHandler(ILambdaContext context)
+    {
+        return "All Employees";
+    }
+
+}
